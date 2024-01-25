44f95007b6b67e23ade24ed281b9bf39ac5b8476
dibborah <dibyajyotiborah469@gmail.com>
2024-01-25 21:56:05 +0530

    intergration with react-query/devtools

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,26 @@
 import ReactDOM from "react-dom/client";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactQueryDevtools } from "react-query/devtools";
 import App from "./App.jsx";
 import Products from "./Products.jsx";
 import Product from "./Product.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+import React from "react";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // Create a client
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 // Setting the fetching behaviours Globally
-const queryClient = new QueryClient({
-    defaultOptions:{
-        queries:{
-            staleTime: 10000
-        }
-    }
-})
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       staleTime: 10000,
+//     },
+//   },
+// });
 
 const router = createBrowserRouter([
   {
@@ -34,8 +38,10 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // Provide the client to your App
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
 );
