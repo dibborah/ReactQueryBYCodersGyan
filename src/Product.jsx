347844eadc282ca6fd9b042b35ad644508d52bec
dibborah <dibyajyotiborah469@gmail.com>
2024-01-26 17:36:10 +0530

    using useMutation and so mutating using put method. mutation.mutate, mutation.isLoading, mutation.isError were used

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,34 +1,62 @@
-import { useQuery } from "react-query";
+import axios from "axios";
+import { useMutation, useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 
 const Product = () => {
-  
-   const params = useParams();
-   console.log(params);
-  
+  const params = useParams();
+
+  // Mutations
+
+  const mutation = useMutation({
+    mutationFn: (newProduct) => {// newProduct is the object that we are going to update
+      return axios.put(`https://dummyjson.com/products/${params.productId}`, newProduct)
+    },
+  })
+
   const fetchProduct = async () => {
-    const response = await fetch(`https://dummyjson.com/products/${params.productId}`);
+    const response = await fetch(
+      `https://dummyjson.com/products/${params.productId}`
+    );
     const data = response.json();
     return data;
-  }
-  const {data: product, isLoading, error} = useQuery({
-    queryKey:["product", params.productId],
+  };
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["product", params.productId],
     queryFn: fetchProduct,
-  })
-  if(isLoading){
-    return <div>Loading...</div>
+  });
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
-  if(error){
-    return <div>{error.message}</div>
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+  if (mutation.isLoading) {
+    return <h2>Updating...</h2>;
+  }
+  if (mutation.isError) {
+    return <h2>Error while updating: {mutation.error.message}</h2>;
   }
   return (
-    <div>Product:{product.title}</div>
-  )
-}
+    <div>
+      <div>Product:{product.title}</div>
+      <button
+            onClick={() => {
+              mutation.mutate({ title: 'Updated Product' })
+            }}
+          >
+            Create Product
+          </button>
+    </div>
+  );
+};
 
 export default Product;
 
-
-// useMutation: 
+// useMutation:
 // Gives us some methods like:
 // 1. mutate 2. isLoading 3. isError 4. isSuccess 5. error
+
